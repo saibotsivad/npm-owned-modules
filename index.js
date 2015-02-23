880480c9514c520959eeaf5ad89bb6a5d331936a
Tobias Davis <accounts@davistobias.com>
2015-02-23 00:47:49 -0600

    Changed travis versions, updated methods, bumped version.

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var cheerio = require('cheerio')
 
 module.exports = function getNpmModules(username, cb) {
 	https.get('https://www.npmjs.com/~' + username, function(res) {
+		var completeData = ''
 		res.setEncoding('utf8')
-		res.on('data', function(body) {
+		res.on('data', function(data) {
+			completeData = completeData + data
+		})
+		res.on('end', function(data) {
+			completeData = completeData + data
 			var modules = []
-			var $ = cheerio.load(body)
+			var $ = cheerio.load(completeData)
 			$('#profile #packages').parent().next().children().each(function(index, element) {
 				$(element).children('a').each(function(i, e) {
 					var href = $(this).attr('href')
